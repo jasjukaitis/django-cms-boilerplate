399f1bde9b65b8c04cc17b6121449501ae5ff8d5
Raphael Jasjukaitis <r.jasjukaitis@raphaa.de>
2012-07-21 20:45:55 +0200

    DB-Path updated.

--- website/settings/development.py
+++ website/settings/development.py
@@ -12,7 +12,7 @@ COMPRESS_PRECOMPILERS = ()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(PROJECT_DIR, 'dev.db'),
+        'NAME': os.path.join(PROJECT_DIR, 'website', 'dev.db'),
     }
 }
 
